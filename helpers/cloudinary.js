5bcac5895c247972531fa8db4055c67e7bba9598
RIYASTM <riyasuddeentm30@gmail.com>
2025-09-30 20:29:00 +0530

    cloudinary 1

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-const cloud_name = process.env.CLOUDINARY_CLOUD_NAME?.trim();
-const api_key = process.env.CLOUDINARY_API_KEY?.trim();
-const api_secret = process.env.CLOUDINARY_API_SECRET?.trim();
+const cloud_name = process.env.CLOUDINARY_NAME?.trim();
+const api_key = process.env.CLOUDINARY_KEY?.trim();
+const api_secret = process.env.CLOUDINARY_SECRET?.trim();
 
 if (!cloud_name || !api_key || !api_secret) {
     throw new Error("Cloudinary environment variables are missing!");
